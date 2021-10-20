7853d62cb85f6f953612c2233c4c98666547930e
Luis Enrique <enrique_inf@hotmail.com>
2021-10-20 16:53:56 -0500

    primer commit

---
+++ BlazorExpenseTracker.Data/SqlConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorExpenseTracker.Data
+{
+    public class SqlConfiguration
+    {
+        public string ConnectionString { get; }
+        public SqlConfiguration(string connectionString) => ConnectionString = connectionString;
+    }
+}
